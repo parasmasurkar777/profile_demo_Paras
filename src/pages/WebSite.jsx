e5990e73c53c8506619ce84b7d9e739a8154b292
Paras Masurkar <parasmasurkar10@gmail.com>
2026-09-12 09:30:36 +0530

    Fix Nav import for deployment

--- src/pages/WebSite.jsx
+++ src/pages/WebSite.jsx
@@ -2,7 +2,7 @@
 import Cont from '../components/Cont'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-import Nav from '../components/nav'
+import Nav from '../components/Nav'
 
 export default function WebSite() {
   return (
